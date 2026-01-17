e4c8a5b685636187e4b2c775689b9dbbe6cc6bd9
William Thomas Shacklett <will.shacklett@gmail.com>
2026-01-17 12:37:37 -0600

    Update __init__.py

--- src/gv_engine/__init__.py
+++ src/gv_engine/__init__.py
@@ -1,8 +1,4 @@
 from .core import Constraint, gv_score
 from .coupling import coupling_drift
 
-__all__ = [
-    "Constraint",
-    "gv_score",
-    "coupling_drift",
-]
+__all__ = ["Constraint", "gv_score", "coupling_drift"]
