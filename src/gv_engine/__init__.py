1aadfc3773acf4a525d3735e35b1b8e4e5c4a8b5
William Thomas Shacklett <will.shacklett@gmail.com>
2026-01-17 12:48:04 -0600

    Update __init__.py

--- src/gv_engine/__init__.py
+++ src/gv_engine/__init__.py
@@ -1,4 +1,19 @@
+"""
+gv_engine
+
+A minimal constraint-based engine for computing GV-style scalar risk and coupling metrics.
+
+Public API:
+- Constraint dataclass
+- gv_score(...) scalar strain score
+- coupling_drift(...) coupling deviation scalar
+"""
+
 from .core import Constraint, gv_score
 from .coupling import coupling_drift
 
-__all__ = ["Constraint", "gv_score", "coupling_drift"]
+__all__ = [
+    "Constraint",
+    "gv_score",
+    "coupling_drift",
+]
